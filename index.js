720ac7b8a627c32160c97acb52a41b5cf379bd06
mayb3lat3r <zaqhaha@gmail.com>
2021-03-13 18:03:22 +0300

    Final

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 import './select/select.scss'
+import {Selector} from "./select/selector";
 
+const themes = [
+    {
+        title: 'First theme',
+        id: 1
+    },
+    {
+        title: 'Second theme',
+        id: 2
+    },
+    {
+        title: 'Third theme',
+        id: 3
+    },
+    {
+        title: 'Fourth theme',
+        id: 4
+    },
+    {
+        title: 'Fifth theme',
+        id: 5
+    },
+    {
+        title: 'Sixth theme',
+        id: 6
+    },
+    {
+        title: 'Seventh theme',
+        id: 7
+    }
+]
 
+const selector = new Selector('#selector', themes)
